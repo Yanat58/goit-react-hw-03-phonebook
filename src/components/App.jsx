1bfe4f5633063f2b628bf3e1a246ada981f2e497
Tetiana Karlova <tanyalyt80@gmail.com>
2023-05-06 01:10:41 +0300

    add modal,  save contact in localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,137 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Notiflix from 'notiflix';
+import { nanoid } from 'nanoid';
+import { BiX, BiUserPlus } from 'react-icons/bi';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from 'components/ContactList/ContactList';
+import { Filter } from 'components/Filter/Filter';
+import { Modal } from 'components/Modal/Modal';
+import css from 'components/App.module.css';
+
+export class App extends Component {
+  state = {
+    // contacts: [
+    //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    // ],
+    contacts: [],
+    filter: '',
+    showModal: false,
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+    console.log(nextContacts);
+    console.log(prevContacts);
+
+    if (nextContacts !== prevContacts) {
+      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+    }
+  }
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    const nameIsExist = contacts.some(
+      contact => contact.name.toLowerCase().trim() === name.toLowerCase().trim()
+    );
+
+    const numberIsExist = contacts.some(
+      contact => contact.number.trim() === number.trim()
+    );
+
+    if (nameIsExist) {
+      Notiflix.Report.warning(`${name} is already in contacts`);
+      return;
+    } else if (numberIsExist) {
+      Notiflix.Report.warning(`${number} is already in contacts`);
+      return;
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+  };
+
+  getVisibleContact = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  render() {
+    const { filter, showModal } = this.state;
+    const visibleContacts = this.getVisibleContact();
+    return (
+      <div className={css.container}>
+        <h1 className={css.titleAplication}>
+          Phone<span className={css.titleColor}>book</span>
+        </h1>
+        <button
+          type="button"
+          className={css.btnAddContact}
+          onClick={this.toggleModal}
+        >
+          <BiUserPlus className={css.addModalIcon} size={25} />
+        </button>
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <h2 className={css.titleSection}>Contact List</h2>
+            <ContactForm onSubmit={this.addContact} />
+            <button
+              className={css.closeBtnModal}
+              type="button"
+              onClick={this.toggleModal}
+            >
+              <BiX className={css.closeBtnIcon} size={25} />
+            </button>
+          </Modal>
+        )}
+
+        <h2 className={css.titleSection}>Contacts</h2>
+
+        <Filter value={filter} onChange={this.changeFilter} />
+
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+import css from './Modal.module.css';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handelKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleClickBackdrop = e => {
+    if (e.target === e.currentTarget) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div className={css.modalBackdrop} onClick={this.handleClickBackdrop}>
+        <div className={css.modalContent}>{this.props.children}</div>
+      </div>,
+      modalRoot
+    );
+  }
+}
